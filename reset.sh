11345b55500eba2132934bdc2f43e359ea89a8cd
Koszeghy David <david.koszeghy@softec.sk>
2017-01-10 10:36:19 +0100

    working version

--- reset.sh
+++ reset.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #Script for reseting iC880A over reset jumper which is connected at GPIO 25
 echo "25" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio25/direction
@@ -7,3 +8,4 @@ sleep 5
 echo "0" > /sys/class/gpio/gpio25/value
 sleep 1
 echo "0" > /sys/class/gpio/gpio25/value
+echo "25" > /sys/class/gpio/unexport
